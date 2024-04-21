efbe0ec149a7b8fcc3110e23d9851ccf445916e7
root <root@LAPTOP-4954DC8J>
2024-04-22 00:44:48 +0300

    fix: norminette miniRT.h

--- includes/miniRT.h
+++ includes/miniRT.h
@@ -6,7 +6,7 @@
 /*   By: tklimova <tklimova@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/25 10:49:49 by tklimova          #+#    #+#             */
-/*   Updated: 2024/04/21 23:30:48 by tklimova         ###   ########.fr       */
+/*   Updated: 2024/04/22 00:44:13 by tklimova         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,13 +33,13 @@
 
 # define M_PI 3.14159265358979323846
 # define EPSILON 0.0001
+# define FLT_MAX 3.402823466e+38F
 
 # include <stdio.h>
 # include <fcntl.h>
 # include <stdlib.h>
 # include <unistd.h>
 # include <math.h>
-# include <float.h>
 # include <limits.h>
 # include "../libft/libft.h"
 # include "../minilibx-linux/mlx.h"
@@ -81,12 +81,6 @@ typedef struct s_ambient_light
 	float	ratio;
 	int		*rgb;	// int[3]
 }				t_ambient_light;
-
-// typedef struct s_mtx
-// {
-// 	float	rotate[16];
-// }				t_mtx;
-
 
 typedef struct s_camera
 {
@@ -155,140 +149,140 @@ typedef struct s_sphere_eq
 	float	root[2];
 }			t_sphere_eq;
 
-
 typedef struct s_closest_obj
 {
 	float		dist;
 	t_g_objects	*obj;
 }				t_closest_obj;
 
+int				is_valid_extension(char *str, char *extantion,
+					t_mini_rt_data *data);
 
-int			is_valid_extension(char *str, char *extantion,
-				t_mini_rt_data *data);
+void			handle_error(int err_no, char *text, t_mini_rt_data *data);
 
-void		handle_error(int err_no, char *text, t_mini_rt_data *data);
+void			init_mini_rt_data(t_mini_rt_data *data);
 
-void		init_mini_rt_data(t_mini_rt_data *data);
+void			ft_read_file(t_mini_rt_data *data);
 
-void		ft_read_file(t_mini_rt_data *data);
+char			*ft_read_substr(t_mini_rt_data *data, int *eol);
 
-char		*ft_read_substr(t_mini_rt_data *data, int *eol);
+void			ft_parse_ambient_light(t_mini_rt_data *data, int *eol);
 
-void		ft_parse_ambient_light(t_mini_rt_data *data, int *eol);
+void			ft_parse_camera(t_mini_rt_data *data, int *eol);
 
-void		ft_parse_camera(t_mini_rt_data *data, int *eol);
+bool			is_float(char *str);
 
-bool		is_float(char *str);
+bool			is_int_in_range(int nb, int min, int max);
 
-bool		is_int_in_range(int nb, int min, int max);
+bool			is_float_in_range(float nb, float min, float max);
 
-bool		is_float_in_range(float nb, float min, float max);
+int				*get_rgb_data(t_mini_rt_data *data, char *rgb);
 
-int			*get_rgb_data(t_mini_rt_data *data, char *rgb);
+int				ft_read_token(char **substr, t_mini_rt_data *data,
+					int *eol, char *err_msg);
 
-int			ft_read_token(char **substr, t_mini_rt_data *data,
-				int *eol, char *err_msg);
+int				ft_read_last_token(char **substr, t_mini_rt_data *data,
+					int *eol, char *err_msg);
 
-int			ft_read_last_token(char **substr, t_mini_rt_data *data,
-				int *eol, char *err_msg);
+float			*get_coords(char *coords, t_mini_rt_data *data);
 
-float		*get_coords(char *coords, t_mini_rt_data *data);
+float			*get_coords_with_range(char *coords, t_mini_rt_data *data,
+					float min, float max);
 
-float		*get_coords_with_range(char *coords, t_mini_rt_data *data,
-				float min, float max);
+void			ft_parse_light(t_mini_rt_data *data, int *eol);
 
-void		ft_parse_light(t_mini_rt_data *data, int *eol);
+void			delete_ambient_light(t_mini_rt_data *data);
 
-void		delete_ambient_light(t_mini_rt_data *data);
+void			delete_camera(t_mini_rt_data *data);
 
-void		delete_camera(t_mini_rt_data *data);
+void			delete_mini_rt_data(t_mini_rt_data *data);
 
-void		delete_mini_rt_data(t_mini_rt_data *data);
+void			delete_light(t_mini_rt_data *data);
 
-void		delete_light(t_mini_rt_data *data);
+t_g_objects		*create_obj(t_mini_rt_data *data, char *token);
 
-t_g_objects	*create_obj(t_mini_rt_data *data, char *token);
+void			push_to_data_obj(t_mini_rt_data *data, t_g_objects *obj);
 
-void		push_to_data_obj(t_mini_rt_data *data, t_g_objects *obj);
+void			delete_obj(t_g_objects *obj);
 
-void		delete_obj(t_g_objects *obj);
+t_g_objects		*prefilled_obj(t_mini_rt_data *data, char *coords, char *rgb,
+					char *token);
 
-t_g_objects	*prefilled_obj(t_mini_rt_data *data, char *coords, char *rgb,
-				char *token);
+void			ft_parse_sphere(t_mini_rt_data *data, int *eol, char *token);
 
-void		ft_parse_sphere(t_mini_rt_data *data, int *eol, char *token);
+void			ft_parse_plane(t_mini_rt_data *data, int *eol, char *token);
 
-void		ft_parse_plane(t_mini_rt_data *data, int *eol, char *token);
+void			ft_parse_cylinder(t_mini_rt_data *data, int *eol, char *token);
 
-void		ft_parse_cylinder(t_mini_rt_data *data, int *eol, char *token);
+void			delete_shapes(t_mini_rt_data *data);
 
-void		delete_shapes(t_mini_rt_data *data);
+void			ft_free_4_str(char *str1, char *str2, char *str3,
+					char *str4);
 
-void		ft_free_4_str(char *str1, char *str2, char *str3,
-				char *str4);
+void			ft_print_data(t_mini_rt_data *data);
 
-void		ft_print_data(t_mini_rt_data *data);
+void			init_img_data(t_img_data	**img_data);
 
-void	init_img_data(t_img_data	**img_data);
+void			free_coords(t_img_data *img_data);
 
-void	free_coords(t_img_data *img_data);
+void			create_win(t_mini_rt_data *data);
 
-void	create_win(t_mini_rt_data *data);
+void			destroy_win(t_mini_rt_data *data);
 
-void	destroy_win(t_mini_rt_data *data);
+int				win_close(int keycode, t_vars *vars);
 
-int	win_close(int keycode, t_vars *vars);
+int				win_destroy(t_vars *vars);
 
-int	win_destroy(t_vars *vars);
-
-int	win_resize(t_vars *vars);
+int				win_resize(t_vars *vars);
 
 // vector_ops
 
-void	vector_add(float *origin, float *direction,	float *result);
+void			vector_add(float *origin, float *direction,	float *result);
 
-void	vector_subtract(float *origin, float *direction, float *result);
+void			vector_subtract(float *origin, float *direction, float *result);
 
-void	vector_multiply(float *origin, float *vector1, float *vector2, \
+void			vector_multiply(float *origin, float *vector1, float *vector2, \
 	float *result);
 
-float	get_dot_product(float *vector_1, float *vector_2);
+float			get_dot_product(float *vector_1, float *vector_2);
 
-void	vector_divide(float *origin, float *vector1, float *vector2, \
+void			vector_divide(float *origin, float *vector1, float *vector2, \
 	float *result);
 
-float	get_vector_length(float *position1, float *position2);
+float			get_vector_length(float *position1, float *position2);
 
-void	normalize_vector(float *origin, float *vector);
+void			normalize_vector(float *origin, float *vector);
 
-void	copy_f_vector(float *src, float *dest);
+void			copy_f_vector(float *src, float *dest);
 
-void	scale_vector(float *vector, float scalar, float *result);
+void			scale_vector(float *vector, float scalar, float *result);
 
-t_vect 	cross_product(t_vect vect_a, t_vect vect_b);
+t_vect			cross_product(t_vect vect_a, t_vect vect_b);
 
-float	*vector_mtx_multy(float *vect, float *mtx, float *mult_vect);
+float			*vector_mtx_multy(float *vect, float *mtx, float *mult_vect);
 
-void	made_precalc(t_mini_rt_data *data);
+void			made_precalc(t_mini_rt_data *data);
 
 // SD functions
 
-float	sd_selector(float *curr_pt, t_mini_rt_data *data, t_g_objects object);
+float			sd_selector(float *curr_pt, t_mini_rt_data *data,
+					t_g_objects object);
 
 // ray tracing functions
 
-void	trace_ray(t_mini_rt_data *data, float *vp_coords);
+void			trace_ray(t_mini_rt_data *data, float *vp_coords);
 
-int		apply_sdf_to_objects(t_mini_rt_data *data, float *current_point);
+int				apply_sdf_to_objects(t_mini_rt_data *data,
+					float *current_point);
 
-void	draw(t_mini_rt_data *data);
+void			draw(t_mini_rt_data *data);
 
-int		rgb_to_hex(int r, int g, int b);
+int				rgb_to_hex(int r, int g, int b);
 
 t_closest_obj	get_closest_obj(t_mini_rt_data *data, t_ray ray);
 
-t_vect	fill_vector(float x, float y, float z);
+t_vect			fill_vector(float x, float y, float z);
 
-int	rgb_to_hex(int r, int g, int b);
+int				rgb_to_hex(int r, int g, int b);
 
 #endif
